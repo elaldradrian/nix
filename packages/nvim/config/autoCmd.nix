ef79d3ef0768c3c342e66082c1263fc43cd78ed7
Rune Dahl Billeskov <rune@dahl-billeskov.com>
2024-12-14 14:40:27 +0100

    feat: detect changes when branch changes.

--- packages/nvim/config/autoCmd.nix
+++ packages/nvim/config/autoCmd.nix
@@ -1,27 +1,35 @@
 {
   autoCmd = [
     {
+      desc = "Check if we need to reload the file when it changed";
+      event = [
+        "FocusGained"
+        "TermClose"
+        "TermLeave"
+      ];
+      callback.__raw = # lua
+        ''
+          function()
+              if vim.o.buftype ~= "nofile" then
+                vim.cmd("checktime")
+              end
+            end
+        '';
+    }
+    {
       desc = "Highlight on yank";
       event = "TextYankPost";
       callback.__raw = # lua
         ''
-          function() vim.highlight.on_yank({ higroup="IncSearch", timeout=250 }) end
+          function() 
+            vim.highlight.on_yank({ higroup="IncSearch", timeout=250 })
+          end
         '';
     }
-    #   event = [
-    #     "BufLeave"
-    #   ];
-    #   callback = {
-    #     __raw = # Lua
-    #       ''
-    #         function()
-    #           if vim.bo.modified then
-    #             require("conform").format()
-    #             vim.cmd("write")
-    #           end
-    #         end
-    #       '';
-    #   };
-    # }
+    {
+      event = "FileType";
+      pattern = "helm";
+      command = "LspRestart";
+    }
   ];
 }
