5d528292ee471461caade41f1afe4161642dd88c
Rune Dahl Billeskov <rune@dahl-billeskov.com>
2025-01-04 10:13:10 +0100

    fix: toggles.

--- home/profiles/rune-laptop.nix
+++ home/profiles/rune-laptop.nix
@@ -6,16 +6,18 @@
 
   home.homeDirectory = "/home/rune";
 
-  features = {
-    desktop.enable = true;
-    devUtils.enable = true;
-    docker.enable = false;
-  };
-  programs = {
-    nvim.enable = true;
-    colima.enable = false;
-    slack.enable = false;
-    dbeaver.enable = false;
-    "1password".enable = true;
+  opt = {
+    features = {
+      desktop.enable = true;
+      devUtils.enable = true;
+      docker.enable = false;
+    };
+    programs = {
+      nvim.enable = true;
+      colima.enable = false;
+      slack.enable = false;
+      dbeaver.enable = false;
+      "1password".enable = true;
+    };
   };
 }
