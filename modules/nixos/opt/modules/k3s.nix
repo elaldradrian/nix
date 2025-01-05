211a08a3c40f29c2809cf2e59501eeee8aa23ae8
Rune Dahl Billeskov <rune@dahl-billeskov.com>
2025-01-05 20:26:12 +0100

    .

--- modules/nixos/opt/modules/k3s.nix
+++ modules/nixos/opt/modules/k3s.nix
@@ -6,7 +6,7 @@
 {
   config = lib.mkIf config.opt.features.k3s.enable {
     services.k3s = {
-      enable = false;
+      enable = true;
       role = "server";
       tokenFile = config.sops.secrets.k3s-token.path;
       clusterInit = config.opt.features.k3s.clusterInit;
