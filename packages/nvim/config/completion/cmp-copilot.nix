d5522a3ddcc26e94e5513eaf2f65519e1d452c01
Rune Dahl Billeskov <rune@dahl-billeskov.com>
2025-01-13 14:50:37 +0100

    feat: use for all filetypes.

--- packages/nvim/config/completion/cmp-copilot.nix
+++ packages/nvim/config/completion/cmp-copilot.nix
@@ -2,6 +2,17 @@
   plugins.copilot-lua = {
     enable = true;
     settings = {
+      filetypes = {
+        "." = true;
+        cvs = true;
+        gitcommit = true;
+        gitrebase = true;
+        help = true;
+        hgcommit = true;
+        markdown = true;
+        svn = true;
+        yaml = true;
+      };
       suggestion = {
         enabled = true;
         auto_trigger = true;
