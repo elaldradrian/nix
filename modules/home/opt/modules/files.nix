dbfd849499e0eeb919ffd9b6bb656ba4a8110d02
Rune Dahl Billeskov <rune@dahl-billeskov.com>
2025-02-12 13:07:46 +0100

    fix: secret and artifactory token.

--- modules/home/opt/modules/files.nix
+++ modules/home/opt/modules/files.nix
@@ -7,13 +7,23 @@
   config = lib.mkIf (config.opt.features.devUtils.enable && config.opt.features.work-machine.enable) {
     sops = {
       secrets.github_token = { };
+      secrets.artifactory_email = { };
+      secrets.artifactory_token = { };
+
       templates.github_token = {
         path = "${config.home.homeDirectory}/.npmrc";
         content = lib.generators.toINIWithGlobalSection { } {
           globalSection = {
             prefix = "${config.home.homeDirectory}/.npm-packages";
             "//npm.pkg.github.com/:_authToken" = config.sops.placeholder.github_token;
-            "@bd-b0100:registry" = "https://npm.pkg.github.com";
+            "@bd-b0100:registry" = "https://npm.pkg.github.com/";
+
+            "always-auth" = "true";
+            "email" = config.sops.placeholder.artifactory_email;
+
+            "//registry.bankdata.dev/artifactory/api/npm/rel-npm/:_authToken" =
+              config.sops.placeholder.artifactory_token;
+            "@mz-js:registry" = "https://registry.bankdata.dev/artifactory/api/npm/rel-npm/";
           };
         };
       };
