127f72b99fe97e4d67c3939e937999dd807207ad
Rune Dahl Billeskov <31374639+elaldradrian@users.noreply.github.com>
2025-03-04 15:00:47 +0100

    feat: tmux pane wrap disabled and swap binding added. (#47)

--- modules/home/opt/tmux.nix
+++ modules/home/opt/tmux.nix
@@ -27,31 +27,20 @@
       # See: https://github.com/christoomey/vim-tmux-navigator
       is_vim="ps -o state= -o comm= -t '#{pane_tty}' \
           | grep -iqE '^[^TXZ ]+ +(\\S+\\/)?g?(view|l?n?vim?x?|fzf)(diff)?$'"
+      bind-key -n 'C-left'  if-shell "$is_vim" { send-keys C-left  } { if-shell -F '#{pane_at_left}'   {} { select-pane -L } }
+      bind-key -n 'C-down'  if-shell "$is_vim" { send-keys C-down  } { if-shell -F '#{pane_at_bottom}' {} { select-pane -D } }
+      bind-key -n 'C-up'    if-shell "$is_vim" { send-keys C-up    } { if-shell -F '#{pane_at_top}'    {} { select-pane -U } }
+      bind-key -n 'C-right' if-shell "$is_vim" { send-keys C-right } { if-shell -F '#{pane_at_right}'  {} { select-pane -R } }
 
-      bind-key -n 'C-left' if-shell "$is_vim" 'send-keys C-left'  'select-pane -L -o'
-      bind-key -n 'C-down' if-shell "$is_vim" 'send-keys C-down'  'select-pane -D -o'
-      bind-key -n 'C-up' if-shell "$is_vim" 'send-keys C-up'  'select-pane -U -o'
-      bind-key -n 'C-right' if-shell "$is_vim" 'send-keys C-right'  'select-pane -R -o'
-
-      tmux_version='$(tmux -V | sed -En "s/^tmux ([0-9]+(.[0-9]+)?).*/\1/p")'
-
-      if-shell -b '[ "$(echo "$tmux_version < 3.0" | bc)" = 1 ]' \
-          "bind-key -n 'C-\\' if-shell \"$is_vim\" 'send-keys C-\\'  'select-pane -l'"
-      if-shell -b '[ "$(echo "$tmux_version >= 3.0" | bc)" = 1 ]' \
-          "bind-key -n 'C-\\' if-shell \"$is_vim\" 'send-keys C-\\\\'  'select-pane -l'"
-
-      bind-key -T copy-mode-vi 'C-left' select-pane -L -o
-      bind-key -T copy-mode-vi 'C-down' select-pane -D -o
-      bind-key -T copy-mode-vi 'C-up' select-pane -U -o
-      bind-key -T copy-mode-vi 'C-right' select-pane -R -o
-      bind-key -T copy-mode-vi 'C-\' select-pane -l
+      bind-key -T copy-mode-vi 'C-left'  if-shell -F '#{pane_at_left}'   {} { select-pane -L }
+      bind-key -T copy-mode-vi 'C-down'  if-shell -F '#{pane_at_bottom}' {} { select-pane -D }
+      bind-key -T copy-mode-vi 'C-up'    if-shell -F '#{pane_at_top}'    {} { select-pane -U }
+      bind-key -T copy-mode-vi 'C-right' if-shell -F '#{pane_at_right}'  {} { select-pane -R }
 
       bind c new-window -c "#{pane_current_path}"
       bind s split-window -c "#{pane_current_path}"
       bind v split-window -h -c "#{pane_current_path}"
 
-      bind -n M-S-Left swap-pane -L
-      bind -n M-S-Right swap-pane -R
       bind -n M-S-Up swap-pane -U
       bind -n M-S-Down swap-pane -D
 
