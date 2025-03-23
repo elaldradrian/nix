24d51473a186e0709cee4833525ddc05132d6825
Rune Dahl Billeskov <rune@dahl-billeskov.com>
2025-03-23 12:59:28 +0100

    feat: add settings to nixos settings.

--- modules/nixos/core/nix.nix
+++ modules/nixos/core/nix.nix
@@ -1,8 +1,10 @@
+{ user, ... }:
 {
   nixpkgs.config.allowUnfree = true;
   nix = {
     settings = {
       experimental-features = "nix-command flakes";
+      trusted-users = [ user ];
     };
   };
 }
