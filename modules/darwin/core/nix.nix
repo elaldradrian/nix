1de0f9f9200b0c4aec0cd511f1c073ba8e732f7d
Rune Dahl Billeskov <31374639+elaldradrian@users.noreply.github.com>
2025-03-23 12:56:33 +0100

    feat: add trusted user. (#63)

--- modules/darwin/core/nix.nix
+++ modules/darwin/core/nix.nix
@@ -1,8 +1,11 @@
+{ user, ... }:
 {
   nixpkgs.config.allowUnfree = true;
   nix = {
     settings = {
       experimental-features = "nix-command flakes";
+      extra-platforms = "x86_64-darwin aarch64-darwin x86_64-linux";
+      trusted-users = [ user ];
     };
   };
 }
