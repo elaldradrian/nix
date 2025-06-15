63f88b0a533e39330d7300883d748492db521271
Rune Dahl Billeskov <31374639+elaldradrian@users.noreply.github.com>
2025-06-15 10:55:53 +0200

    feat: allow hackage cache. (#90)

--- modules/darwin/core/nix.nix
+++ modules/darwin/core/nix.nix
@@ -4,8 +4,14 @@
   nix = {
     settings = {
       experimental-features = "nix-command flakes";
-      extra-platforms = "x86_64-darwin aarch64-darwin x86_64-linux";
+      extra-platforms = "x86_64-darwin aarch64-darwin";
       trusted-users = [ user ];
+      trusted-public-keys = [
+        "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+      ];
+      substituters = [
+        "https://cache.iog.io"
+      ];
     };
   };
 }
