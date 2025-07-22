87a3d968d3bda18473653d8783a249133bbac69f
Rune Dahl Billeskov <31374639+elaldradrian@users.noreply.github.com>
2025-07-22 09:58:02 +0200

    feat: keymap. (#100)

--- packages/nvim/config/completion/blink.nix
+++ packages/nvim/config/completion/blink.nix
@@ -1,68 +1,109 @@
 {
+  plugins.blink-cmp-dictionary.enable = true;
+  plugins.blink-cmp-git.enable = true;
+  plugins.blink-emoji.enable = true;
+  plugins.blink-cmp-spell.enable = true;
+  plugins.blink-ripgrep.enable = true;
+
   plugins.blink-cmp = {
     enable = true;
-    settings.sources = {
-      default = [
-        "lsp"
-        "path"
-        "luasnip"
-        "buffer"
-        "dictionary"
-        "emoji"
-        "git"
-        "spell"
-        "ripgrep"
-      ];
-      providers = {
-        dictionary = {
-          module = "blink-cmp-dictionary";
-          name = "Dict";
-          score_offset = 100;
-          min_keyword_length = 3;
+    settings = {
+      appearance = {
+        appearance.kind_icons = {
+          Class = "󱡠";
+          Color = "󰏘";
+          Constant = "󰏿";
+          Constructor = "󰒓";
+          Enum = "󰦨";
+          EnumMember = "󰦨";
+          Event = "󱐋";
+          Field = "󰜢";
+          File = "󰈔";
+          Folder = "󰉋";
+          Function = "󰊕";
+          Interface = "󱡠";
+          Keyword = "󰻾";
+          Method = "󰊕";
+          Module = "󰅩";
+          Operator = "󰪚";
+          Property = "󰖷";
+          Reference = "󰬲";
+          Snippet = "󱄽";
+          Struct = "󱡠";
+          Text = "󰉿";
+          TypeParameter = "󰬛";
+          Unit = "󰪚";
+          Value = "󰦨";
+          Variable = "󰆦";
         };
-        emoji = {
-          module = "blink-emoji";
-          name = "Emoji";
-          score_offset = 15;
-          opts = {
-            insert = true;
+      };
+      keymap = {
+        "<tab>" = [
+          "accept"
+        ];
+      };
+      sources = {
+        default = [
+          "lsp"
+          "path"
+          "buffer"
+          "dictionary"
+          "emoji"
+          "git"
+          "spell"
+          "ripgrep"
+        ];
+        providers = {
+          dictionary = {
+            module = "blink-cmp-dictionary";
+            name = "Dict";
+            score_offset = 100;
+            min_keyword_length = 3;
           };
-        };
-        git = {
-          module = "blink-cmp-git";
-          name = "git";
-          score_offset = 100;
-          opts = {
-            commit = { };
-            git_centers = {
-              git_hub = { };
+          emoji = {
+            module = "blink-emoji";
+            name = "Emoji";
+            score_offset = 15;
+            opts = {
+              insert = true;
             };
           };
-        };
-        spell = {
-          module = "blink-cmp-spell";
-          name = "Spell";
-          score_offset = 100;
-          opts = {
+          git = {
+            module = "blink-cmp-git";
+            name = "git";
+            score_offset = 100;
+            opts = {
+              commit = { };
+              git_centers = {
+                git_hub = { };
+              };
+            };
           };
-        };
-        ripgrep = {
-          async = true;
-          module = "blink-ripgrep";
-          name = "Ripgrep";
-          score_offset = 100;
-          opts = {
-            prefix_min_len = 3;
-            context_size = 5;
-            max_filesize = "1M";
-            project_root_marker = ".git";
-            project_root_fallback = true;
-            search_casing = "--ignore-case";
-            additional_rg_options = { };
-            fallback_to_regex_highlighting = true;
-            ignore_paths = { };
-            additional_paths = { };
-            debug = false;
+          spell = {
+            module = "blink-cmp-spell";
+            name = "Spell";
+            score_offset = 100;
+            opts = {
+            };
+          };
+          ripgrep = {
+            async = true;
+            module = "blink-ripgrep";
+            name = "Ripgrep";
+            score_offset = 100;
+            opts = {
+              prefix_min_len = 3;
+              context_size = 5;
+              max_filesize = "1M";
+              project_root_marker = ".git";
+              project_root_fallback = true;
+              search_casing = "--ignore-case";
+              additional_rg_options = { };
+              fallback_to_regex_highlighting = true;
+              ignore_paths = { };
+              additional_paths = { };
+              debug = false;
+            };
           };
         };
       };
--- packages/nvim/config/completion/default.nix
+++ packages/nvim/config/completion/default.nix
@@ -2,8 +2,6 @@
   imports = [
     ./blink.nix
     ./copilot.nix
-    ./lspkind.nix
-    ./luasnip.nix
     ./schemastore.nix
   ];
 }
