85c0d3af8d2884b33c7dfef899c93a15bbafa41b
Rune Dahl Billeskov <rune@dahl-billeskov.com>
2025-07-25 14:16:35 +0200

    chore: bump.

--- flake.nix
+++ flake.nix
@@ -52,7 +52,6 @@
             packages = with pkgs; [
               nixfmt-rfc-style
               git
-              nh
             ];
             name = "dots";
             DIRENV_LOG_FORMAT = "";
