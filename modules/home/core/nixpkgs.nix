cc830fca4cfa9f12d14e425fa3752ae1b775dbf3
Rune Dahl Billeskov <rune@dahl-billeskov.com>
2025-08-07 10:12:11 +0200

    feat: add stable pkgs to nvim.

--- modules/home/core/nixpkgs.nix
+++ modules/home/core/nixpkgs.nix
@@ -1,3 +1,20 @@
+{ inputs, user, ... }:
 {
-  nixpkgs.config.allowUnfree = true;
+  nixpkgs = {
+    config.allowUnfree = true;
+    overlays = [
+      (self: _super: {
+        stable = import inputs.nixpkgs-stable {
+          inherit (self) system;
+          config.allowUnfree = true;
+        };
+      })
+    ];
+  };
+  nix = {
+    settings = {
+      experimental-features = "nix-command flakes";
+      trusted-users = [ user ];
+    };
+  };
 }
