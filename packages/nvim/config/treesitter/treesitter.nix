12f3d9e47ffe66e4539976d225d738d1175299ae
Rune Dahl Billeskov <rune@dahl-billeskov.com>
2025-09-04 20:24:28 +0200

    feat: rest -> kulala.

--- packages/nvim/config/treesitter/treesitter.nix
+++ packages/nvim/config/treesitter/treesitter.nix
@@ -1,11 +1,12 @@
+{ pkgs, ... }:
 {
   plugins.treesitter = {
     enable = true;
+    grammarPackages = pkgs.vimPlugins.nvim-treesitter.allGrammars;
     settings = {
       indent.enable = true;
       highlight.enable = true;
     };
     folding = true;
-    nixvimInjections = true;
   };
 }
