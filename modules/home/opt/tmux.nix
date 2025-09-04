ca3274b18ad099256352adebd7f3c27bf43958bb
Rune Dahl Billeskov <31374639+elaldradrian@users.noreply.github.com>
2025-09-04 20:58:48 +0200

    feat: add hjkl motions where arrows are available. (#124)

--- modules/home/opt/tmux.nix
+++ modules/home/opt/tmux.nix
@@ -41,26 +41,45 @@
       set -s extended-keys on
       set-option -g renumber-windows on
 
+      # Enter copy mode with Prefix+v
+      bind v copy-mode
+      unbind [
+
       # Smart pane switching with awareness of Vim splits.
       # See: https://github.com/christoomey/vim-tmux-navigator
       is_vim="ps -o state= -o comm= -t '#{pane_tty}' \
           | grep -iqE '^[^TXZ ]+ +(\\S+\\/)?g?(view|l?n?vim?x?|fzf)(diff)?$'"
+
       # Move
       bind-key -n 'C-Left'  if-shell "$is_vim" { send-keys C-Left  } { if-shell -F '#{pane_at_left}'   {} { select-pane -L } }
       bind-key -n 'C-Down'  if-shell "$is_vim" { send-keys C-Down  } { if-shell -F '#{pane_at_bottom}' {} { select-pane -D } }
       bind-key -n 'C-Up'    if-shell "$is_vim" { send-keys C-Up    } { if-shell -F '#{pane_at_top}'    {} { select-pane -U } }
       bind-key -n 'C-Right' if-shell "$is_vim" { send-keys C-Right } { if-shell -F '#{pane_at_right}'  {} { select-pane -R } }
 
-      bind-key -T copy-mode-vi 'C-Left'  if-shell -F '#{pane_at_left}'   {} { select-pane -L }
-      bind-key -T copy-mode-vi 'C-Down'  if-shell -F '#{pane_at_bottom}' {} { select-pane -D }
-      bind-key -T copy-mode-vi 'C-Up'    if-shell -F '#{pane_at_top}'    {} { select-pane -U }
-      bind-key -T copy-mode-vi 'C-Right' if-shell -F '#{pane_at_right}'  {} { select-pane -R }
+      bind-key -n 'C-h'  if-shell "$is_vim" { send-keys C-h  } { if-shell -F '#{pane_at_left}'   {} { select-pane -L } }
+      bind-key -n 'C-j'  if-shell "$is_vim" { send-keys C-j  } { if-shell -F '#{pane_at_bottom}' {} { select-pane -D } }
+      bind-key -n 'C-k'    if-shell "$is_vim" { send-keys C-k    } { if-shell -F '#{pane_at_top}'    {} { select-pane -U } }
+      bind-key -n 'C-l' if-shell "$is_vim" { send-keys C-l } { if-shell -F '#{pane_at_right}'  {} { select-pane -R } }
 
-      bind-key -n 'M-C-Left'  resize-pane -L 2
-      bind-key -n 'M-C-Down'  resize-pane -D 2
-      bind-key -n 'M-C-Up'    resize-pane -U 2
-      bind-key -n 'M-C-Right' resize-pane -R 2
+      # In copy-mode, we need to use different bindings
+      # bind-key -T copy-mode-vi 'C-Left'  if-shell -F '#{pane_at_left}'   {} { select-pane -L }
+      # bind-key -T copy-mode-vi 'C-Down'  if-shell -F '#{pane_at_bottom}' {} { select-pane -D }
+      # bind-key -T copy-mode-vi 'C-Up'    if-shell -F '#{pane_at_top}'    {} { select-pane -U }
+      # bind-key -T copy-mode-vi 'C-Right' if-shell -F '#{pane_at_right}'  {} { select-pane -R }
 
+      # Vim-aware resize
+      bind-key -n 'M-C-Left'  if-shell "$is_vim" { send-keys M-C-Left  } { resize-pane -L 2 }
+      bind-key -n 'M-C-Down'  if-shell "$is_vim" { send-keys M-C-Down  } { resize-pane -D 2 }
+      bind-key -n 'M-C-Up'    if-shell "$is_vim" { send-keys M-C-Up    } { resize-pane -U 2 }
+      bind-key -n 'M-C-Right' if-shell "$is_vim" { send-keys M-C-Right } { resize-pane -R 2 }
+
+      # Optional hjkl equivalents
+      bind-key -n 'M-C-h' if-shell "$is_vim" { send-keys M-C-h } { resize-pane -L 2 }
+      bind-key -n 'M-C-j' if-shell "$is_vim" { send-keys M-C-j } { resize-pane -D 2 }
+      bind-key -n 'M-C-k' if-shell "$is_vim" { send-keys M-C-k } { resize-pane -U 2 }
+      bind-key -n 'M-C-l' if-shell "$is_vim" { send-keys M-C-l } { resize-pane -R 2 }
+
+      bind-key -n 'M-C-t' new-session -c "#{pane_current_path}"
       bind-key -n 'M-C-c' new-window -c "#{pane_current_path}"
       bind-key -n 'M-C-s' split-window -c "#{pane_current_path}"
       bind-key -n 'M-C-v' split-window -h -c "#{pane_current_path}"
@@ -68,7 +87,10 @@
       bind-key -n 'M-C-PageDown' swap-pane -D
       bind-key -n 'M-C-PageUp'   swap-pane -U
 
-      bind-key -n 'M-C-t' list-sessions
+      bind-key -n 'M-C-l' choose-tree -s
+
+      bind-key -n M-C-n next-window
+      bind-key -n M-C-p previous-window
 
       set -g status-right '#{cpu_bg_color} CPU: #{cpu_icon} #{cpu_percentage} | %a %h-%d %H:%M '
       run-shell ${pkgs.tmuxPlugins.cpu}/share/tmux-plugins/cpu/cpu.tmux
--- packages/nvim/config/mini/move.nix
+++ packages/nvim/config/mini/move.nix
@@ -1,17 +1,172 @@
 {
   plugins.mini.modules.move = {
-    mappings = {
-      left = "<M-left>";
-      right = "<M-right>";
-      down = "<M-down>";
-      up = "<M-up>";
-
-      line_left = "<M-left>";
-      line_right = "<M-right>";
-      line_down = "<M-down>";
-      line_up = "<M-up>";
-    };
-
+    mappings = { };
     options.reindent_linewise = true;
   };
+  keymaps = [
+    # Move selection
+    {
+      mode = [
+        "x"
+      ];
+      key = "<M-h>";
+      action = "<cmd>lua MiniMove.move_selection(\"left\")<cr>";
+      options = {
+        desc = "Move selection left";
+      };
+    }
+    {
+      mode = [
+        "x"
+      ];
+      key = "<M-j>";
+      action = "<cmd>lua MiniMove.move_selection(\"up\")<cr>";
+      options = {
+        desc = "Move selection up";
+      };
+    }
+    {
+      mode = [
+        "x"
+      ];
+      key = "<M-l>";
+      action = "<cmd>lua MiniMove.move_selection(\"down\")<cr>";
+      options = {
+        desc = "Move selection down";
+      };
+    }
+    {
+      mode = [
+        "x"
+      ];
+      key = "<M-l>";
+      action = "<cmd>lua MiniMove.move_selection(\"right\")<cr>";
+      options = {
+        desc = "Move selection right";
+      };
+    }
+
+    {
+      mode = [
+        "x"
+      ];
+      key = "<M-left>";
+      action = "<cmd>lua MiniMove.move_selection(\"left\")<cr>";
+      options = {
+        desc = "Move selection left";
+      };
+    }
+    {
+      mode = [
+        "x"
+      ];
+      key = "<M-up>";
+      action = "<cmd>lua MiniMove.move_selection(\"up\")<cr>";
+      options = {
+        desc = "Move selection up";
+      };
+    }
+    {
+      mode = [
+        "x"
+      ];
+      key = "<M-down>";
+      action = "<cmd>lua MiniMove.move_selection(\"down\")<cr>";
+      options = {
+        desc = "Move selection down";
+      };
+    }
+    {
+      mode = [
+        "x"
+      ];
+      key = "<M-right>";
+      action = "<cmd>lua MiniMove.move_selection(\"right\")<cr>";
+      options = {
+        desc = "Move selection right";
+      };
+    }
+    # Move line
+    {
+      mode = [
+        "n"
+      ];
+      key = "<M-h>";
+      action = "<cmd>lua MiniMove.move_line(\"left\")<cr>";
+      options = {
+        desc = "Move line left";
+      };
+    }
+    {
+      mode = [
+        "n"
+      ];
+      key = "<M-j>";
+      action = "<cmd>lua MiniMove.move_line(\"up\")<cr>";
+      options = {
+        desc = "Move selection up";
+      };
+    }
+    {
+      mode = [
+        "n"
+      ];
+      key = "<M-l>";
+      action = "<cmd>lua MiniMove.move_line(\"down\")<cr>";
+      options = {
+        desc = "Move selection down";
+      };
+    }
+    {
+      mode = [
+        "n"
+      ];
+      key = "<M-l>";
+      action = "<cmd>lua MiniMove.move_line(\"right\")<cr>";
+      options = {
+        desc = "Move selection right";
+      };
+    }
+
+    {
+      mode = [
+        "n"
+      ];
+      key = "<M-left>";
+      action = "<cmd>lua MiniMove.move_line(\"left\")<cr>";
+      options = {
+        desc = "Move selection left";
+      };
+    }
+    {
+      mode = [
+        "n"
+      ];
+      key = "<M-up>";
+      action = "<cmd>lua MiniMove.move_line(\"up\")<cr>";
+      options = {
+        desc = "Move selection up";
+      };
+    }
+    {
+      mode = [
+        "n"
+      ];
+      key = "<M-down>";
+      action = "<cmd>lua MiniMove.move_line(\"down\")<cr>";
+      options = {
+        desc = "Move selection down";
+      };
+    }
+    {
+      mode = [
+        "n"
+      ];
+      key = "<M-right>";
+      action = "<cmd>lua MiniMove.move_line(\"right\")<cr>";
+      options = {
+        desc = "Move selection right";
+      };
+    }
+  ];
 }
