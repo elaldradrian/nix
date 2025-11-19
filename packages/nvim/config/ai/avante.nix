d40c0528cbc26f3d84153d201266d7c0ee7e71de
Rune Dahl Billeskov <rune@dahl-billeskov.com>
2025-11-19 08:32:13 +0100

    fix: theme.

--- packages/nvim/config/ai/avante.nix
+++ packages/nvim/config/ai/avante.nix
@@ -1,39 +1,29 @@
 {
   plugins.avante = {
     enable = true;
-    # lazyLoad.settings.event = "DeferredUIEnter";
+    lazyLoad.settings.event = "DeferredUIEnter";
     settings = {
+      mode = "legacy";
       provider = "copilot";
-      system_prompt.__raw = # Lua
-        ''
-          function()
-            local hub = require("mcphub").get_hub_instance()
-            return hub and hub:get_active_servers_prompt() or ""
-          end
-        '';
-      custom_tools.__raw = # Lua
-        ''
-          function()
-            return {
-                require("mcphub.extensions.avante").mcp_tool(),
-            }
-          end
-        '';
+      # system_prompt.__raw = # Lua
+      #   ''
+      #     function()
+      #       local hub = require("mcphub").get_hub_instance()
+      #       return hub and hub:get_active_servers_prompt() or ""
+      #     end
+      #   '';
+      # custom_tools.__raw = # Lua
+      #   ''
+      #     function()
+      #       return {
+      #           require("mcphub.extensions.avante").mcp_tool(),
+      #       }
+      #     end
+      #   '';
       behaviour = {
-        auto_apply_diff_after_generation = false;
+        auto_set_highlight_group = false;
+        auto_approve_tool_permissions = false;
       };
-      disabled_tools = [
-        "list_files"
-        "search_files"
-        "read_file"
-        "create_file"
-        "rename_file"
-        "delete_file"
-        "create_dir"
-        "rename_dir"
-        "delete_dir"
-        "bash"
-      ];
       input = {
         provider = "snacks";
         provider_opts = {
