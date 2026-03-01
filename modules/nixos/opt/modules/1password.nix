daa2bf6cfd549dba6bfd96fca72e9b59d99cadc2
Rune Dahl Billeskov <rune@dahl-billeskov.com>
2026-03-01 11:32:43 +0100

    feat: always install 1password.

--- modules/nixos/opt/modules/1password.nix
+++ modules/nixos/opt/modules/1password.nix
@@ -1,10 +1,22 @@
-{ config, lib, ... }:
 {
-  config = lib.mkIf config.opt.features.desktop.enable {
-    programs._1password.enable = true;
-    programs._1password-gui = {
-      enable = true;
-      polkitPolicyOwners = [ "rune" ];
-    };
-  };
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  config = lib.mkMerge [
+    {
+      programs._1password.enable = true;
+      environment.systemPackages = with pkgs; [
+        _1password-cli
+      ];
+    }
+    (lib.mkIf config.opt.features.desktop.enable {
+      programs._1password-gui = {
+        enable = true;
+        polkitPolicyOwners = [ "rune" ];
+      };
+    })
+  ];
 }
