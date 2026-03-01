4000a6853093c28e872230013c72eec740a871f6
Rune Dahl Billeskov <31374639+elaldradrian@users.noreply.github.com>
2026-03-01 10:18:11 +0100

    chore: bump. (#167)

--- packages/nvim/config/lsp/conform.nix
+++ packages/nvim/config/lsp/conform.nix
@@ -8,6 +8,7 @@
     google-java-format
     libxml2
     markdownlint-cli2
+    pgformatter
   ];
 
   plugins.conform-nvim = {
@@ -69,6 +70,7 @@
         nix = [ "nixfmt" ];
         scss = [ "prettierd" ];
         sh = [ "shfmt" ];
+        sql = [ "pg_format" ];
         terraform = {
           lsp_format = "prefer";
         };
