4222a3b0c3f1fcce67223c984c92a555d4d044b1
Rune Dahl Billeskov <rune@dahl-billeskov.com>
2026-04-01 10:37:59 +0200

    feat: ai

--- modules/home/opt/programs/llama-swap.nix
+++ modules/home/opt/programs/llama-swap.nix
@@ -24,14 +24,18 @@ let
         if pkgs.stdenv.isDarwin then
           {
             "qwen3.5-35b-a3b" = {
-              cmd = "${llama-server} --port \${PORT} --model /var/lib/llama-swap/models/Qwen3.5-35B-A3B-UD-IQ4_XS.gguf --jinja -c 65536 -ngl 99 --flash-attn on --cache-type-k q4_0 --cache-type-v q4_0 --no-warmup --parallel 1 --batch-size 4096 --keep -1";
+              cmd = "${llama-server} --port \${PORT} --model /var/lib/llama-swap/models/Qwen3.5-35B-A3B-UD-IQ4_XS.gguf --jinja -c 65536 --cache-type-k q4_0 --cache-type-v q4_0 --no-warmup --parallel 1 --batch-size 4096 --keep -1 --temp 0.6";
               ttl = 900;
             };
           }
         else
           {
             "qwen3.5-9b" = {
-              cmd = "${llama-server} --port \${PORT} --model /var/lib/llama-swap/models/Qwen3.5-9B-UD-Q4_K_XL.gguf --jinja -c 65536 -ngl 99 --flash-attn on --cache-type-k q4_0 --cache-type-v q4_0 --no-warmup --parallel 1 --batch-size 4096 --keep -1";
+              cmd = "${llama-server} --port \${PORT} --model /var/lib/llama-swap/models/Qwen3.5-9B-UD-Q4_K_XL.gguf --jinja -c 65536 --cache-type-k q4_0 --cache-type-v q4_0 --no-warmup --parallel 1 --batch-size 4096 --keep -1 --temp 0.6";
+              ttl = 900;
+            };
+            "qwen3.5-35b-a3b" = {
+              cmd = "${llama-server} --port \${PORT} --model /var/lib/llama-swap/models/Qwen3.5-35B-A3B-UD-IQ2_XXS.gguf --jinja -c 8000 --cache-type-k q4_0 --cache-type-v q4_0 --no-warmup --parallel 1 --batch-size 4096 --keep -1 --temp 0.6";
               ttl = 900;
             };
           };
