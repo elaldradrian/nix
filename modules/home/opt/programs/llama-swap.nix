cf8e5fbaf2dcbfa57a0e91bc79f381219bcddb3a
Rune Dahl Billeskov <rune@dahl-billeskov.com>
2026-04-02 12:21:31 +0200

    feat: AI

--- modules/home/opt/programs/llama-swap.nix
+++ modules/home/opt/programs/llama-swap.nix
@@ -34,11 +34,11 @@ let
         else
           {
             "qwen3.5-9b" = {
-              cmd = "${llama-server} --port \${PORT} --model /var/lib/llama-swap/models/Qwen3.5-9B-UD-Q4_K_XL.gguf --jinja -c 65536 --cache-type-k q4_0 --cache-type-v q4_0 --no-warmup --parallel 1 --batch-size 4096 --keep -1 --temp 0.6";
+              cmd = "${llama-server} --port \${PORT} --model /var/lib/llama-swap/models/Qwen3.5-9B-UD-Q4_K_XL.gguf --jinja -c 65536 --cache-type-k q4_0 --cache-type-v q4_0 --no-warmup --parallel 1 --keep -1 --temp 0.6";
               ttl = 900;
             };
             "qwen3.5-35b-a3b" = {
-              cmd = "${llama-server} --port \${PORT} --model /var/lib/llama-swap/models/Qwen3.5-35B-A3B-UD-IQ2_XXS.gguf --jinja -c 65536 --cache-type-k q4_0 --cache-type-v q4_0 --no-warmup --parallel 1 --batch-size 4096 --keep -1 --temp 0.6";
+              cmd = "${llama-server} --port \${PORT} --model /var/lib/llama-swap/models/Qwen3.5-35B-A3B-UD-IQ2_XXS.gguf --jinja -c 65536 --cache-type-k q4_0 --cache-type-v q4_0 --no-warmup --parallel 1 --keep -1 --temp 0.6";
               ttl = 900;
             };
 
--- packages/nvim/config/ai/codecompanion.nix
+++ packages/nvim/config/ai/codecompanion.nix
@@ -29,11 +29,10 @@
                   },
                   schema = {
                     model = {
-                      default = 'qwen3.5';
+                      default = 'qwen3.5-35b-a3b';
                       choices = {
                         ["qwen3.5-9b"] = { opts = { can_reason = false } },
                         "qwen3.5-35b-a3b",
-                        "qwen3.5",
                         "qwen3.5-122b-a10b",
                       },
                     },
