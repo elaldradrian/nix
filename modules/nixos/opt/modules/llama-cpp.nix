3c9b823ec1cb725118c82952bf7ee483d9030683
Rune Dahl Billeskov <rune@dahl-billeskov.com>
2026-05-26 21:22:27 +0200

    chore: bump

--- modules/nixos/opt/modules/llama-cpp.nix
+++ modules/nixos/opt/modules/llama-cpp.nix
@@ -7,8 +7,8 @@ let
   llama-pkgs =
     let
       newSrc = pkgs.llama-cpp.src.override {
-        tag = "b9193";
-        hash = "sha256-HuYRPe2owXw0lLrX4hsfszmNxpG1H2/kroB2IeEBzVM=";
+        tag = "b9334";
+        hash = "sha256-/Cgmzb5B/t3HtE25XpmpsfI0A1IkVt5qGGpQvRuFzVk=";
       };
     in
     (pkgs.llama-cpp.override {
@@ -16,33 +16,33 @@ let
       rocmSupport = true;
     }).overrideAttrs
       (prev: {
-        version = "9193";
+        version = "9334";
         npmRoot = "tools/ui";
         src = newSrc;
         npmDeps = pkgs.fetchNpmDeps {
-          name = "llama-cpp-9193-npm-deps";
+          name = "llama-cpp-9334-npm-deps";
           inherit (prev) patches;
           src = newSrc;
           preBuild = ''
             pushd tools/ui
           '';
-          hash = "sha256-WaEePrEZ7O/7deP2KJhe0AwiSKYA8HOqETmMHUkmBe0=";
+          hash = "sha256-Iyg8FpcTKf2UYHuK7mA3cTAqVaLcQPcS0YCa5Qf01Gc=";
         };
       });
 
   llama-server = "${llama-pkgs}/bin/llama-server";
 
   modelsIni = pkgs.writeText "models.ini" (
-    lib.generators.toINI { listsAsDuplicateKeys = true; } {
+    lib.generators.toINI { } {
       "qwen3.6-27b" = {
         model = "/var/lib/llama/models/Qwen3.6-27B-Q6_K.gguf";
         jinja = "true";
         chat-template-kwargs = ''{"preserve_thinking": true}'';
         no-warmup = "true";
-        ctx-checkpoints = "80";
-        checkpoint-every-n-tokens = "4096";
-        cache-ram = "25000";
-        ctx-size = "120000";
+        ctx-checkpoints = "30";
+        checkpoint-min-step = "256";
+        cache-ram = "30000";
+        ctx-size = "240000";
         # f16: 26.2 gb vs q8_0: 22.5 gb
         # 734/49.6     vs 681/51.8
         cache-type-k = "q8_0";
@@ -52,7 +52,7 @@ let
         keep = "-1";
         min-p = "0.00";
         no-mmap = "true";
-        parallel = "1";
+        parallel = "2";
         presence-penalty = "0";
         repeat-penalty = "1";
         temp = "0.6";
@@ -60,17 +60,8 @@ let
         top-p = "0.95";
         batch-size = "2048";
         ubatch-size = "512";
-        spec-type = [
-          "draft-mtp"
-          # "ngram-mod"
-          # "ngram-map-k4v"
-        ];
+        spec-type = "draft-mtp";
         spec-draft-n-max = "2";
-        # spec-ngram-mod-n-match = "24";
-        # spec-ngram-mod-n-min = "48";
-        # spec-ngram-mod-n-max = "64";
-        # spec-ngram-map-k4v-size-n = "12";
-        # spec-ngram-map-k4v-size-m = "48";
         no-mmproj-offload = "true";
         device = "Vulkan0";
       };
@@ -83,7 +74,7 @@ let
         chat-template-kwargs = ''{"preserve_thinking": true}'';
         no-warmup = "true";
         ctx-checkpoints = "80";
-        checkpoint-every-n-tokens = "6144";
+        checkpoint-min-step = "256";
         cache-ram = "25000";
         fit = "off";
         ngl = 99;
