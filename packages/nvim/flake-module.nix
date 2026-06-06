6389bb4ad368a95a1a17cd14ca331496a4af6634
Rune Dahl Billeskov <rune@dahl-billeskov.com>
2026-06-06 20:50:30 +0200

    ch

--- packages/nvim/flake-module.nix
+++ packages/nvim/flake-module.nix
@@ -18,11 +18,24 @@
     let
       inherit (inputs.nixvim.legacyPackages.${system}) makeNixvimWithModule;
       inherit (inputs.nixvim.lib.${system}.check) mkTestDerivationFromNvim;
+
+      # Temporary: nixpkgs master ships a stale FOD hash for kulala-core's
+      # bun-built node_modules. Pin to the hash our build actually produces.
+      # Drop this once upstream re-pins (check `nix flake update nixpkgs` first).
+      kulalaOverlay = final: prev: {
+        kulala-core = prev.kulala-core.overrideAttrs (old: {
+          node_modules = old.node_modules.overrideAttrs (_: {
+            outputHash = "sha256-XQlBawD3vt8pVc7Gy9XeiGie89HWbljNJt7kUEDaDKk=";
+          });
+        });
+      };
+
+      patchedPkgs = pkgs.extend kulalaOverlay;
     in
     {
       # Run using `nix run .#nvim`
       packages.nvim = makeNixvimWithModule {
-        inherit pkgs;
+        pkgs = patchedPkgs;
         module = self.nixvimModules.default;
         extraSpecialArgs = {
           inherit self inputs;
