efb6148152b7c40a124b16e7fe20c2d82ab96a49
Rune Dahl Billeskov <rune@dahl-billeskov.com>
2026-06-28 15:35:38 +0200

    feat: set prefix for devUtils

--- modules/home/opt/programs/npmrc.nix
+++ modules/home/opt/programs/npmrc.nix
@@ -5,23 +5,34 @@
   ...
 }:
 {
-  config = lib.mkIf (config.opt.features.devUtils.enable && config.opt.features.work-machine.enable) {
-    sops = {
-      secrets.artifactory_token = { };
-      secrets.artifactory_url = { };
-
-      templates.npmrc = {
+  config = lib.mkMerge [
+    (lib.mkIf config.opt.features.devUtils.enable {
+      sops.templates.npmrc = {
         path = "${homeDir}/.npmrc";
         content = lib.generators.toINIWithGlobalSection { } {
-          globalSection = {
-            prefix = "${homeDir}/.npm-packages";
+          globalSection.prefix = "${homeDir}/.npm-packages";
+        };
+      };
+    })
 
-            "//registry.bankdata.dev/artifactory/api/npm/rel-npm/:_authToken" =
-              config.sops.placeholder.artifactory_token;
-            "registry" = config.sops.placeholder.artifactory_url;
+    (lib.mkIf config.opt.features.work-machine.enable {
+      sops = {
+        secrets.artifactory_token = { };
+        secrets.artifactory_url = { };
+
+        templates.npmrc = {
+          path = "${homeDir}/.npmrc";
+          content = lib.generators.toINIWithGlobalSection { } {
+            globalSection = {
+              prefix = "${homeDir}/.npm-packages";
+
+              "//registry.bankdata.dev/artifactory/api/npm/rel-npm/:_authToken" =
+                config.sops.placeholder.artifactory_token;
+              "registry" = config.sops.placeholder.artifactory_url;
+            };
           };
         };
       };
-    };
-  };
+    })
+  ];
 }
